6c2a8074fd43472854cdea0a5810f6abaf549493
Rupanzil <rupanzilmamunprince@gmail.com>
2026-02-24 09:10:01 +0530

    Add CORS middleware to FastAPI app

--- todoapp/main.py
+++ todoapp/main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI, Depends, HTTPException, Path
+from fastapi.middleware.cors import CORSMiddleware
 
 import models
 from database import engine
 from routers import auth, todos, admin, users
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Creates the database and the table
 models.Base.metadata.create_all(bind=engine)
